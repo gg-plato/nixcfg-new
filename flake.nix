9bd31befb13a8826d5e6d1a890ccce7e49ba4a6f
Greg <platongrigore@gmail.com>
2025-10-31 14:41:47 +0000

    update

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
-    # cosmic-unstable.url = "github:ninelore/nixpkgs-cosmic-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    cosmic-unstable.url = "github:ninelore/nixpkgs-cosmic-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -21,7 +21,7 @@
       home-manager,
       nixos-hardware,
       nix-flatpak,
-      # cosmic-unstable,
+      cosmic-unstable,
       catppuccin,
       ...
     }@inputs:
@@ -38,13 +38,13 @@
           # so the old configuration file still takes effect
           ./configuration.nix
           nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen4
-          # {
-            # nix.settings = {
-              # substituters = [ "https://9lore.cachix.org/" ];
-              # trusted-public-keys = [ "9lore.cachix.org-1:H2/a1Wlm7VJRfJNNvFbxtLQPYswP3KzXwSI5ROgzGII=" ];
-            # };
-          # }
-          # cosmic-unstable.nixosModules.default
+          {
+            nix.settings = {
+              substituters = [ "https://9lore.cachix.org/" ];
+              trusted-public-keys = [ "9lore.cachix.org-1:H2/a1Wlm7VJRfJNNvFbxtLQPYswP3KzXwSI5ROgzGII=" ];
+            };
+          }
+          cosmic-unstable.nixosModules.default
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
